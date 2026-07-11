e0566ac69a43fd6aa2b9db94f6722702e6385061
Harsimranjot Aulakh <114257365+harsimrana@users.noreply.github.com>
2026-07-10 20:20:23 -0600

    add: week 10 day 03 Objects
      1. By creating an object literal
      2. Constructor function and using protype object
      3. Troubleshooting

--- demos/Weekly Material/Week 10/Demo 01/js/code.js
+++ demos/Weekly Material/Week 10/Demo 01/js/code.js
@@ -14,12 +14,12 @@ window.onload = function ()
             - properties/attributes/data members
             - and functions/members
     Create your own objects
-    1. By creating a object literal
+    1. By creating an object literal
     2. Constructor function and using protype object
 
     */
 
-    // First method
+    // First method by creating an object literal
      student = {
         // properties of object
         studentId : 1,
@@ -38,11 +38,55 @@ window.onload = function ()
     // To access properties or attributes use dot(.) operator
     console.log(student);
 
-    document.querySelector("main").innerHTML = student.studentId +" "+ student.studentName;
+    document.querySelector("main").innerHTML += student.studentId +" "+ student.studentName;
 
-    document.querySelector("main").innerHTML = `${student.studentId}  ${student.studentName}`;
+    document.querySelector("main").innerHTML += `${student.studentId}  ${student.studentName}`;
 
+
+    //2. Creating objects using constructor
+
+    var instructor = function (name, id, ImagePath)
+    {
+        // validations before assigning the value to the property
+        this.instructorName = name;
+        this.instructorId   = id;
     
+        // creating an image object inside Instructor Object
+        this.instructorImage = new Image();  // Creating an image object
+        this.instructorImage.src= ImagePath; // Assign image path to src property
+
+    }
+
+    // Instructor is an object type and you can create instances of it
+
+    // To add functions into object , use constructor prototype
+    instructor.prototype.changeName =  function (name)
+    {
+        // perform some validation checks on the value coming from client 
+        this.instructorName= name;
+        // this keyword will refer to current object
+
+    }
+
+
+    let instructor1 = new instructor("Simran", "1004758", "./images/simran.jpg");
+    let instructor2 = new instructor("Shane", "1004759", "./images/shane.jpg");
+    let instructor3 = new instructor("Herb", "1004760", "./images/Herb.jpg");
+
+    console.log(instructor1);
+
+    instructor1.changeName("Harsimran");
+
+    console.log("After name change " + instructor1.instructorName);
+
+    instructor1.instructorImage.src= "../../Week07/Day03Demo01/images/NaitLogo.png";// Image Path
+    
+    let divContainer = document.querySelector("#InstructorInfo");
+    console.log(divContainer);
+
+
+    document.querySelector("#InstructorInfo").innerHTML =`<p>${instructor1.instructorName}</p>`;
+    divContainer.appendChild(instructor1.instructorImage)
 }
 
 function ChangeName()
